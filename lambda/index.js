bb537ddcee86ec14a4323cd5154f9940d5e886ad
Ahmad Kanawi <ahmad@mosey.systems>
2019-07-21 18:02:31 -0700

    working

--- lambda/index.js
+++ lambda/index.js
@@ -2,21 +2,24 @@
 const doc = require("dynamodb-doc");
 const dynamo = new doc.DynamoDB();
 const AWS = require("aws-sdk");
+const querystring = require("querystring");
 
 exports.handler = (event, context, callback) => {
   // console.log('Received event:', JSON.stringify(event, null, 2));
 
   // Code template courtesy of AWS Tutorials 👍
-  const done = err => {
+  const done = (err, data) => {
     if (err) {
       console.error(err);
     }
 
     return callback(null, {
       statusCode: err ? "400" : "200",
-      body: err ? "something went wrong 😭" : "ok 👍",
+      body: err
+        ? "something went wrong 😭"
+        : succesTemplate(data, "Corvid Security"),
       headers: {
-        "Content-Type": "text/plain; charset=utf8"
+        "Content-Type": "text/html; charset=utf8"
       }
     });
   };
@@ -33,16 +36,19 @@ exports.handler = (event, context, callback) => {
 function handlePost(event, done) {
   let parsedBody = {};
   try {
-    parsedBody = JSON.parse(event.body);
+    parsedBody = querystring.parse(event.body);
   } catch (e) {
     done(e);
+    return;
   }
+
+  // console.log('parsed body:', JSON.stringify(parsedBody, null, 2));
 
   const name = parsedBody.name || "An Anonymous Hacker";
   const email = parsedBody.email || "fake@protonmail.com";
-  const company = parsedBody.company || "";
-  const phone = parsedBody.phone || "";
-  const message = parsedBody.message || "";
+  const company = parsedBody.company || "US Gov't.";
+  const phone = parsedBody.phone || "867-5309";
+  const message = parsedBody.message || "pongHi";
 
   var sns = new AWS.SNS();
   const snsParams = {
@@ -75,7 +81,13 @@ function handlePost(event, done) {
     };
 
     console.log("created dynamoParams:", JSON.stringify(dynamoParams, null, 2));
-    dynamo.putItem(dynamoParams, done);
+    dynamo.putItem(dynamoParams, err => {
+      if (err) {
+        done(err);
+      }
+
+      done(null, { name, email, company, phone, message });
+    });
   });
 }
 
@@ -85,4 +97,40 @@ Email: ${email}
 ${company && `Company: ${company}`}
 ${phone && `Phone: ${phone}`}
 Message: "${message}"
-`
+`;
+
+const succesTemplate = (data, siteName) =>
+  `<html>
+        <head>
+        <title>Thanks for visiting ${siteName}</title>
+        <style>
+        body {
+          font-family: sans-serif;
+          background-color: #0f0f0f;
+          color: white;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+        }
+        pre {
+          background-color: 2a2a2a;
+          border: 1px dotted black;
+          border-radius: 0.25rem;
+          padding: 0.5rem;
+          max-width: 50%;
+          margin-left: 1rem;
+        }
+        footer {
+          position: relative;
+          top: 5rem;
+        }
+        </style>
+        </head>
+        <body>
+        <h4>Thanks for reaching out. We'll be in touch soon. Please save this page for your records:</h4>
+        <pre>${JSON.stringify(data, null, 2)}</pre>
+        <footer>
+        <a href="https://www.corvidsec.com">Back to the site</a>
+        </footer>
+        </body>
+        </html>`;
